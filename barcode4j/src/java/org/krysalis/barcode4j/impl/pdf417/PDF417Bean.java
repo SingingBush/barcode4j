607cce170e005692c12355cb5dfae5f60e7befd2
Jeremias Märki <jmaerki@users.sourceforge.net>
2007-07-11 07:35:53 +0000

    Added auto-sizing feature for PDF417 with an option to specify the width-to-height ration for the symbol. Submitted by: Branko van Abel <branko.at.edmond.nl>
    git-svn-id: svn://svn.code.sf.net/p/barcode4j/svn/trunk@233 2a1e1039-aae1-41f3-a3dd-b9301d3baa95

--- barcode4j/src/java/org/krysalis/barcode4j/impl/pdf417/PDF417Bean.java
+++ barcode4j/src/java/org/krysalis/barcode4j/impl/pdf417/PDF417Bean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Jeremias Maerki.
+ * Copyright 2006-2007 Jeremias Maerki.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  */
 package org.krysalis.barcode4j.impl.pdf417;
 
+import java.awt.Dimension;
+
 import org.krysalis.barcode4j.BarcodeDimension;
 import org.krysalis.barcode4j.TwoDimBarcodeLogicHandler;
 import org.krysalis.barcode4j.impl.AbstractBarcodeBean;
@@ -26,7 +28,7 @@ import org.krysalis.barcode4j.tools.UnitConv;
 /**
  * This class is an implementation of the PDF417 barcode.
  * 
- * @version $Id: PDF417Bean.java,v 1.3 2007-04-13 18:35:14 jmaerki Exp $
+ * @version $Id: PDF417Bean.java,v 1.4 2007-07-11 07:35:52 jmaerki Exp $
  */
 public class PDF417Bean extends AbstractBarcodeBean {
 
@@ -39,93 +41,124 @@ public class PDF417Bean extends AbstractBarcodeBean {
     /** The default column count for PDF417. */
     protected static final int DEFAULT_COLUMN_COUNT = 2;
 
+    /** The default error correction level for PDF417 */
     protected static final int DEFAULT_ERROR_CORRECTION_LEVEL = 0;
-
-    private int errorCorrectionLevel = DEFAULT_ERROR_CORRECTION_LEVEL;
-    private int cols = DEFAULT_COLUMN_COUNT;
-    private Double quietZoneVertical;
     
+    private static final double DEFAULT_WIDTH_TO_HEIGHT_RATIO = 3;
+    private static final int MAX_ROW_COUNT = 90;
+    private static final int MIN_ROW_COUNT = 3;
+    private static final int MIN_COLUMN_COUNT = 1;
+    private static final int MAX_COLUMN_COUNT = 30;
+
+    private int minRows = MIN_ROW_COUNT;
+    private int maxRows = MAX_ROW_COUNT;
+    private int minCols = MIN_COLUMN_COUNT;
+    private int maxCols = MAX_COLUMN_COUNT;
+    private double widthToHeightRatio = DEFAULT_WIDTH_TO_HEIGHT_RATIO;
+    private int errorCorrectionLevel = DEFAULT_ERROR_CORRECTION_LEVEL;
+
+    private Double quietZoneVertical;
+
     /** Create a new instance. */
     public PDF417Bean() {
         this.moduleWidth = DEFAULT_MODULE_WIDTH;
         this.height = DEFAULT_X_TO_Y_FACTOR * moduleWidth;
         this.quietZone = 2 * moduleWidth;
+
+        setColumns(DEFAULT_COLUMN_COUNT);
     }
-    
     /**
      * @see org.krysalis.barcode4j.BarcodeGenerator#generateBarcode(CanvasProvider, String)
      */
     public void generateBarcode(CanvasProvider canvas, String msg) {
-        if ((msg == null) 
-                || (msg.length() == 0)) {
+        if ((msg == null) || (msg.length() == 0)) {
             throw new NullPointerException("Parameter msg must not be empty");
         }
 
-        TwoDimBarcodeLogicHandler handler = 
-                new DefaultTwoDimCanvasLogicHandler(this, new Canvas(canvas));
+        TwoDimBarcodeLogicHandler handler = new DefaultTwoDimCanvasLogicHandler(
+                this, new Canvas(canvas));
 
-        PDF417LogicImpl impl = new PDF417LogicImpl();
-        impl.generateBarcodeLogic(handler, msg, 
-                getColumns(), getErrorCorrectionLevel());
+        PDF417LogicImpl.generateBarcodeLogic(handler, msg, this);
     }
-    
+
+
     /**
      * @see org.krysalis.barcode4j.BarcodeGenerator#calcDimensions(String)
      */
     public BarcodeDimension calcDimensions(String msg) {
-        int k = PDF417ErrorCorrection.getErrorCorrectionCodewordCount(getErrorCorrectionLevel()); 
-        String highLevel = PDF417HighLevelEncoder.encodeHighLevel(msg);
-        int m = highLevel.length();
-        int r = PDF417LogicImpl.getNumberOfRows(m, k, getColumns());
-        
-        double width = (17 * getColumns() + 69) * getModuleWidth();
-        double height = (getBarHeight() * r); 
-        double qzh = (hasQuietZone() ? getQuietZone() : 0);        
-        double qzv = (hasQuietZone() ? getVerticalQuietZone() : 0);        
-        return new BarcodeDimension(width, height, 
-                width + (2 * qzh), height + (2 * qzv), 
+
+        int sourceCodeWords = PDF417HighLevelEncoder.encodeHighLevel(msg).length();
+        Dimension dimension = PDF417LogicImpl.determineDimensions(this,
+                sourceCodeWords);
+
+        if (dimension == null) {
+            throw new IllegalArgumentException("Unable to fit message in columns");
+        }
+
+        double width = (17 * dimension.width + 69) * getModuleWidth();
+        double height = (getBarHeight() * dimension.height);
+        double qzh = (hasQuietZone() ? getQuietZone() : 0);
+        double qzv = (hasQuietZone() ? getVerticalQuietZone() : 0);
+        return new BarcodeDimension(width, height,
+                width + (2 * qzh), height + (2 * qzv),
                 qzh, qzv);
     }
 
-    /**
-     * Sets the error correction level for the barcode.
-     * @param level the error correction level (a value between 0 and 8)
-     */
-    public void setErrorCorrectionLevel(int level) {
-        if (level < 0 || level > 8) {
-            throw new IllegalArgumentException("Error correction level must be between 0 and 8!");
-        }
-        this.errorCorrectionLevel = level;
+    /** @see org.krysalis.barcode4j.impl.AbstractBarcodeBean#getBarWidth(int) */
+    public double getBarWidth(int width) {
+        return width * moduleWidth;
     }
-    
+
+    /** @return the number of data columns to produce */
+    public int getColumns() {
+        return minCols;
+    }
+
     /** @return the error correction level (0-8) */
     public int getErrorCorrectionLevel() {
         return this.errorCorrectionLevel;
     }
-    
+
     /**
-     * Sets the number of data columns for the barcode. The number of rows will automatically
-     * be determined based on the amount of data.
-     * @param cols the number of columns
+     * Gets the maxCols.
+     * @return Returns the maxCols.
      */
-    public void setColumns(int cols) {
-        this.cols = cols;
+    public int getMaxCols() {
+        return maxCols;
     }
-    
-    /** @return the number of data columns to produce */
-    public int getColumns() {
-        return this.cols;
-    }
-    
+
     /**
-     * Sets the height of the vertical quiet zone. If this value is not explicitely set the
-     * vertical quiet zone has the same width as the horizontal quiet zone.
-     * @param height the height of the vertical quiet zone (in mm)
+     * Gets the maximum number of columns.
+     * @return Returns the maximum number of columns.
      */
-    public void setVerticalQuietZone(double height) {
-        this.quietZoneVertical = new Double(height);
+    public int getMaxRows() {
+        return maxRows;
     }
-    
+
+    /**
+     * Gets the minimum number of columns.
+     * @return Returns the minimum number of columns.
+     */
+    public int getMinCols() {
+        return minCols;
+    }
+
+    /**
+     * Gets the minimum number of rows.
+     * @return Returns the minimum number of rows.
+     */
+    public int getMinRows() {
+        return minRows;
+    }
+
+    /**
+     * Returns the height of the rows.
+     * @return the row height (in mm)
+     */
+    public double getRowHeight() {
+        return getBarHeight();
+    }
+
     /** @see org.krysalis.barcode4j.impl.AbstractBarcodeBean#getVerticalQuietZone() */
     public double getVerticalQuietZone() {
         if (this.quietZoneVertical != null) {
@@ -135,19 +168,87 @@ public class PDF417Bean extends AbstractBarcodeBean {
         }
     }
 
-    /** @see org.krysalis.barcode4j.impl.AbstractBarcodeBean#getBarWidth(int) */
-    public double getBarWidth(int width) {
-        return width * moduleWidth;
+    /**
+     * Gets the ratio of the barcode width to the height.
+     * e.g. a ratio of 5 means the width is 5 times the height
+     * @return Returns the ratio of the barcode width to the height
+     */
+    public double getWidthToHeightRatio() {
+        return widthToHeightRatio;
+    }
+
+    private void checkValidColumnCount(int cols) {
+        if (cols < MIN_COLUMN_COUNT || cols > MAX_COLUMN_COUNT) {
+            throw new IllegalArgumentException(
+                    "The number of columns must be between 1 and 30");
+        }
+    }
+
+    private void checkValidRowCount(int rows) {
+        if (rows < MIN_ROW_COUNT || rows > MAX_ROW_COUNT) {
+            throw new IllegalArgumentException(
+                    "The number of rows must be between 3 and 90");
+        }
+    }
+
+    /**
+     * Sets the number of data columns for the barcode. The number of rows will automatically
+     * be determined based on the amount of data.
+     * @param cols the number of columns
+     */
+    public void setColumns(int cols) {
+        setMinCols(cols);
+        setMaxCols(cols);
     }
     
     /**
-     * Returns the height of the rows.
-     * @return the row height (in mm)
+     * Sets the error correction level for the barcode.
+     * @param level the error correction level (a value between 0 and 8)
      */
-    public double getRowHeight() {
-        return getBarHeight();
+    public void setErrorCorrectionLevel(int level) {
+        if (level < 0 || level > 8) {
+            throw new IllegalArgumentException(
+                    "Error correction level must be between 0 and 8!");
+        }
+        this.errorCorrectionLevel = level;
     }
-    
+
+    /**
+     * Sets the maximum number of columns.
+     * @param maxCols the maximum number of columns..
+     */
+    public void setMaxCols(int maxCols) {
+        checkValidColumnCount(maxCols);
+        this.maxCols = maxCols;
+    }
+
+    /**
+     * Sets the maximum number of rows.
+     * @param maxRows the maximum number of rows.
+     */
+    public void setMaxRows(int maxRows) {
+        checkValidRowCount(maxRows);
+        this.maxRows = maxRows;
+    }
+
+    /**
+     * Sets the minimum number of columns.
+     * @param minCols The minimum number of columns.
+     */
+    public void setMinCols(int minCols) {
+        checkValidColumnCount(minCols);
+        this.minCols = minCols;
+    }
+
+    /**
+     * Sets the minimum of rows.
+     * @param minRows the minimum of rows to set.
+     */
+    public void setMinRows(int minRows) {
+        checkValidRowCount(minRows);
+        this.minRows = minRows;
+    }
+
     /**
      * Sets the height of the rows.
      * @param height the height of the rows (in mm)
@@ -155,5 +256,23 @@ public class PDF417Bean extends AbstractBarcodeBean {
     public void setRowHeight(double height) {
         setBarHeight(height);
     }
-    
+
+    /**
+     * Sets the height of the vertical quiet zone. If this value is not explicitely set the
+     * vertical quiet zone has the same width as the horizontal quiet zone.
+     * @param height the height of the vertical quiet zone (in mm)
+     */
+    public void setVerticalQuietZone(double height) {
+        this.quietZoneVertical = new Double(height);
+    }
+
+    /**
+     * Sets the ratio of the barcode width to the height.
+     * e.g. a ratio of 5 means the width is 5 times the height
+     * @param widthToHeightRatio the ratio of the barcode width to the height
+     */
+    public void setWidthToHeightRatio(double widthToHeightRatio) {
+        this.widthToHeightRatio = widthToHeightRatio;
+    }
+
 }
--- barcode4j/src/java/org/krysalis/barcode4j/impl/pdf417/PDF417LogicImpl.java
+++ barcode4j/src/java/org/krysalis/barcode4j/impl/pdf417/PDF417LogicImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Jeremias Maerki.
+ * Copyright 2006-2007 Jeremias Maerki.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
-/* $Id: PDF417LogicImpl.java,v 1.2 2006-11-07 16:03:17 jmaerki Exp $ */
+/* $Id: PDF417LogicImpl.java,v 1.3 2007-07-11 07:35:53 jmaerki Exp $ */
 
 package org.krysalis.barcode4j.impl.pdf417;
+
+import java.awt.Dimension;
 
 import org.krysalis.barcode4j.BarGroup;
 import org.krysalis.barcode4j.ClassicBarcodeLogicHandler;
@@ -25,7 +27,7 @@ import org.krysalis.barcode4j.TwoDimBarcodeLogicHandler;
 /**
  * Top-level class for the logic part of the PDF417 implementation.
  * 
- * @version $Id: PDF417LogicImpl.java,v 1.2 2006-11-07 16:03:17 jmaerki Exp $
+ * @version $Id: PDF417LogicImpl.java,v 1.3 2007-07-11 07:35:53 jmaerki Exp $
  */
 public class PDF417LogicImpl {
 
@@ -39,25 +41,41 @@ public class PDF417LogicImpl {
      * @return the number of rows in the symbol (r)
      */
     public static int getNumberOfRows(int m, int k, int c) {
+        int r = calculateNumberOfRows(m, k, c);
+        if (r > 90) {
+            throw new IllegalArgumentException(
+                    "The message doesn't fit in the configured symbol size."
+                        + " The resultant number of rows for this barcode exceeds 90."
+                        + " Please increase the number of columns or decrease the error correction"
+                        + " level to reduce the number of rows.");
+        }
+        if (r < 3) {
+            throw new IllegalArgumentException(
+                    "The message is too short for the configured symbol size."
+                        + " The resultant number of rows is less than 3."
+                        + " Please decrease the number of columns or increase the error correction"
+                        + " level to increase the number of rows.");
+        }
+        return r;
+    }
+
+    /**
+     * Calculates the necessary number of rows as described in annex Q of ISO/IEC 15438:2001(E).
+     * @param m the number of source codewords prior to the additional of the Symbol Length
+     *          Descriptor and any pad codewords
+     * @param k the number of error correction codewords
+     * @param c the number of columns in the symbol in the data region (excluding start, stop and
+     *          row indicator codewords)
+     * @return the number of rows in the symbol (r)
+     */
+    public static int calculateNumberOfRows(int m, int k, int c) {
         int r = ((m + 1 + k) / c) + 1;
         if (c * r >= (m + 1 + k + c)) {
             r--;
         }
-        if (r > 90) {
-            throw new IllegalArgumentException(
-                    "The resultant number of rows for this barcode exceeds 90."
-                    + " Please increase the number of columns or decrease the error correction"
-                    + " level to reduce the number of rows.");
-        }
-        if (r < 3) {
-            throw new IllegalArgumentException(
-                    "The resultant number of rows is less than 3."
-                    + " Please decrease the number of columns or increase the error correction"
-                    + " level to increase the number of rows.");
-        }
         return r;
     }
-    
+
     /**
      * Calculates the number of pad codewords as described in 4.9.2 of ISO/IEC 15438:2001(E). 
      * @param m the number of source codewords prior to the additional of the Symbol Length
@@ -76,7 +94,7 @@ public class PDF417LogicImpl {
             return 0;
         }
     }
-    
+
     /**
      * Calculates the number of data codewords (equals the Symbol Length Descriptor).
      * @param m the number of source codewords prior to the additional of the Symbol Length
@@ -91,8 +109,8 @@ public class PDF417LogicImpl {
         int r = getNumberOfRows(m, k, c);
         return c * r - k;
     }
-    
-    private void encodeChar(int pattern, int len, ClassicBarcodeLogicHandler logic) {
+
+    private static void encodeChar(int pattern, int len, ClassicBarcodeLogicHandler logic) {
         int map = 1 << len - 1;
         boolean last = (pattern & map) != 0; //Initialize to inverse of first bit
         int width = 0;
@@ -109,9 +127,9 @@ public class PDF417LogicImpl {
         }
         logic.addBar(last, width);
     }
-    
-    private void encodeLowLevel(String fullCodewords, int c, int r, int errorCorrectionLevel, 
-            TwoDimBarcodeLogicHandler logic) {
+
+    private static void encodeLowLevel(String fullCodewords, int c, int r,
+            int errorCorrectionLevel, TwoDimBarcodeLogicHandler logic) {
         int idx = 0;
         for (int y = 0; y < r; y++) {
             int cluster = (y % 3);
@@ -119,7 +137,7 @@ public class PDF417LogicImpl {
             logic.startBarGroup(BarGroup.START_CHARACTER, null);
             encodeChar(PDF417Constants.START_PATTERN, 17, logic);
             logic.endBarGroup();
-            
+
             int left, right;
             if (cluster == 0) {
                 left = (30 * (y / 3)) + ((r - 1) / 3);
@@ -132,12 +150,12 @@ public class PDF417LogicImpl {
                 right = (30 * (y / 3)) + (errorCorrectionLevel * 3) + ((r - 1) % 3);
             }
             int pattern;
-            
+
             logic.startBarGroup(BarGroup.MSG_CHARACTER, null);
             pattern = PDF417Constants.CODEWORD_TABLE[cluster][left];
             encodeChar(pattern, 17, logic);
             logic.endBarGroup();
-            
+
             for (int x = 0; x < c; x++) {
                 logic.startBarGroup(BarGroup.MSG_CHARACTER, null);
                 pattern = PDF417Constants.CODEWORD_TABLE[cluster][fullCodewords.charAt(idx)];
@@ -145,12 +163,12 @@ public class PDF417LogicImpl {
                 logic.endBarGroup();
                 idx++;
             }
-            
+
             logic.startBarGroup(BarGroup.MSG_CHARACTER, null);
             pattern = PDF417Constants.CODEWORD_TABLE[cluster][right];
             encodeChar(pattern, 17, logic);
             logic.endBarGroup();
-            
+
             logic.startBarGroup(BarGroup.STOP_CHARACTER, null);
             encodeChar(PDF417Constants.STOP_PATTERN, 18, logic);
             logic.endBarGroup();
@@ -162,24 +180,40 @@ public class PDF417LogicImpl {
      * Generates the barcode logic.
      * @param logic the logic handler to receive generated events
      * @param msg the message to encode
-     * @param cols the number of columns
-     * @param errorCorrectionLevel the error correction level (value between 0 and 8) 
+     * @param pdf417Bean reference to the PDF417 bean for configuration access
      */
-    public void generateBarcodeLogic(TwoDimBarcodeLogicHandler logic, String msg, 
-            int cols, int errorCorrectionLevel) {
-        if (cols < 1 || cols > 30) {
-            throw new IllegalArgumentException("The number of columns must be between 1 and 30");
-        }
-        
+    public static void generateBarcodeLogic(TwoDimBarcodeLogicHandler logic,
+            String msg, PDF417Bean pdf417Bean) {
+
+        int errorCorrectionLevel = pdf417Bean.getErrorCorrectionLevel();
+
         //1. step: High-level encoding
-        int k = PDF417ErrorCorrection.getErrorCorrectionCodewordCount(errorCorrectionLevel); 
+        int errorCorrectionCodeWords = PDF417ErrorCorrection.getErrorCorrectionCodewordCount(
+                errorCorrectionLevel);
         String highLevel = PDF417HighLevelEncoder.encodeHighLevel(msg);
-        int m = highLevel.length();
-        int r = getNumberOfRows(m, k, cols);
-        int pad = getNumberOfPadCodewords(m, k, cols, r);
-        
+        int sourceCodeWords = highLevel.length();
+
+        Dimension dimension = determineDimensions(pdf417Bean, sourceCodeWords);
+
+        if (dimension == null) {
+            throw new IllegalArgumentException(
+                    "Unable to fit message in columns");
+        }
+
+        int rows = dimension.height;
+        int cols = dimension.width;
+        int pad = getNumberOfPadCodewords(sourceCodeWords,
+                errorCorrectionCodeWords, cols, rows);
+
         //2. step: construct data codewords
-        int n = getNumberOfDataCodewords(m, errorCorrectionLevel, cols);
+        int n = getNumberOfDataCodewords(sourceCodeWords, errorCorrectionLevel,
+                cols);
+        if (n > 929) {
+            throw new IllegalArgumentException(
+                    "Encoded message contains to many code words, message to big ("
+                            + msg.length() + " bytes)");
+        }
+
         StringBuffer sb = new StringBuffer(n);
         sb.append((char)n);
         sb.append(highLevel);
@@ -192,11 +226,63 @@ public class PDF417LogicImpl {
         String ec = PDF417ErrorCorrection.generateErrorCorrection(
                 dataCodewords, errorCorrectionLevel);
         String fullCodewords = dataCodewords + ec;
-        
+
         //4. step: low-level encoding
         logic.startBarcode(msg, msg);
-        encodeLowLevel(fullCodewords, cols, r, errorCorrectionLevel, logic);
+        encodeLowLevel(fullCodewords, cols, rows, errorCorrectionLevel, logic);
         logic.endBarcode();
     }
 
+    /**
+     * Determine optimal nr of columns and rows for the specified number of 
+     * codewords. 
+     * @param pdf417Bean contains configuration settings
+     * @param sourceCodeWords number of code words
+     * @return dimension object containing cols as width and rows as height
+     */
+    public static Dimension determineDimensions(PDF417Bean pdf417Bean,
+            int sourceCodeWords) {
+
+        int minCols = pdf417Bean.getMinCols();
+        int maxCols = pdf417Bean.getMaxCols();
+        int maxRows = pdf417Bean.getMaxRows();
+        int minRows = pdf417Bean.getMinRows();
+        double preferredRatio = pdf417Bean.getWidthToHeightRatio();
+
+        double ratio = 0;
+        Dimension dimension = null;
+        int errorCorrectionCodeWords = PDF417ErrorCorrection.getErrorCorrectionCodewordCount(
+                pdf417Bean.getErrorCorrectionLevel());
+
+        for (int cols = minCols; cols <= maxCols; cols++) {
+
+            int rows = calculateNumberOfRows(sourceCodeWords,
+                    errorCorrectionCodeWords, cols);
+
+            if (rows < minRows) {
+                break;
+            }
+
+            if (rows > maxRows) {
+                continue;
+            }
+
+            double newRatio = ((17 * cols + 69) * pdf417Bean.getModuleWidth())
+                    / (rows * pdf417Bean.getBarHeight());
+
+            // ignore if previous ratio is closer to preferred ratio
+            if (dimension != null
+                    && Math.abs(newRatio - preferredRatio) > Math.abs(ratio
+                            - preferredRatio)) {
+                continue;
+            }
+
+            ratio = newRatio;
+            dimension = new Dimension(cols, rows);
+
+        }
+
+        return dimension;
+    }
 }
+
