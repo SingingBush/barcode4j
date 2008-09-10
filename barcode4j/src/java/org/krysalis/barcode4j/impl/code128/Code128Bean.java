875476f7055a6a2697e7a9606efcedad8f16e274
Jeremias Märki <jmaerki@users.sourceforge.net>
2008-09-10 12:53:19 +0000

    Issue #2082330: Fixed bad error message in Code128Bean.
    git-svn-id: svn://svn.code.sf.net/p/barcode4j/svn/trunk@273 2a1e1039-aae1-41f3-a3dd-b9301d3baa95

--- barcode4j/src/java/org/krysalis/barcode4j/impl/code128/Code128Bean.java
+++ barcode4j/src/java/org/krysalis/barcode4j/impl/code128/Code128Bean.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2005 Jeremias Maerki.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,9 +25,9 @@ import org.krysalis.barcode4j.output.CanvasProvider;
 
 /**
  * This class is an implementation of the Code 128 barcode.
- * 
+ *
  * @author Jeremias Maerki
- * @version $Id: Code128Bean.java,v 1.7 2008-05-13 13:00:45 jmaerki Exp $
+ * @version $Id: Code128Bean.java,v 1.8 2008-09-10 12:53:19 jmaerki Exp $
  */
 public class Code128Bean extends AbstractBarcodeBean {
 
@@ -40,14 +40,14 @@ public class Code128Bean extends AbstractBarcodeBean {
         setQuietZone(10 * this.moduleWidth);
         setVerticalQuietZone(0); //1D barcodes don't have vertical quiet zones
     }
-    
+
     /**
      * @see org.krysalis.barcode4j.impl.AbstractBarcodeBean#hasFontDescender()
      */
     protected boolean hasFontDescender() {
         return true;
     }
-    
+
     /**
      * @see org.krysalis.barcode4j.impl.AbstractBarcodeBean#getBarWidth(int)
      */
@@ -55,7 +55,7 @@ public class Code128Bean extends AbstractBarcodeBean {
         if ((width >= 1) && (width <= 4)) {
             return width * moduleWidth;
         } else {
-            throw new IllegalArgumentException("Only widths 1 and 2 allowed");
+            throw new IllegalArgumentException("Only widths 1, 2, 3 and 4 allowed");
         }
     }
 
@@ -67,8 +67,8 @@ public class Code128Bean extends AbstractBarcodeBean {
         int msgLen = impl.createEncodedMessage(msg).length + 1;
         final double width = ((msgLen * 11) + 13) * getModuleWidth();
         final double qz = (hasQuietZone() ? quietZone : 0);
-        return new BarcodeDimension(width, getHeight(), 
-                width + (2 * qz), getHeight(), 
+        return new BarcodeDimension(width, getHeight(),
+                width + (2 * qz), getHeight(),
                 quietZone, 0.0);
     }
 
@@ -80,10 +80,10 @@ public class Code128Bean extends AbstractBarcodeBean {
             throw new NullPointerException("Parameter msg must not be empty");
         }
 
-        ClassicBarcodeLogicHandler handler = 
+        ClassicBarcodeLogicHandler handler =
                 new DefaultCanvasLogicHandler(this, new Canvas(canvas));
         //handler = new LoggingLogicHandlerProxy(handler);
-        
+
         Code128LogicImpl impl = new Code128LogicImpl();
         impl.generateBarcodeLogic(handler, msg);
     }
