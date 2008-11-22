f203ff4e92bf865af9aa2f0193022b32955897fc
Jeremias Märki <jmaerki@users.sourceforge.net>
2008-11-22 09:57:29 +0000

    Additional test for displayStartStop feature.
    git-svn-id: svn://svn.code.sf.net/p/barcode4j/svn/trunk@280 2a1e1039-aae1-41f3-a3dd-b9301d3baa95

--- barcode4j/src/test/java/org/krysalis/barcode4j/impl/CodabarTest.java
+++ barcode4j/src/test/java/org/krysalis/barcode4j/impl/CodabarTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 Jeremias Maerki.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ import org.krysalis.barcode4j.impl.codabar.CodabarLogicImpl;
 
 /**
  * Test class for the Codabar implementation.
- * 
+ *
  * @author Jeremias Maerki
- * @version $Id: CodabarTest.java,v 1.4 2008-03-31 12:14:54 jmaerki Exp $
+ * @version $Id: CodabarTest.java,v 1.5 2008-11-22 09:57:29 jmaerki Exp $
  */
 public class CodabarTest extends TestCase {
 
@@ -49,14 +49,14 @@ public class CodabarTest extends TestCase {
         } catch (IllegalArgumentException iae) {
             //must fail
         }
-        
+
     }
 
     public void testLogic() throws Exception {
         StringBuffer sb = new StringBuffer();
         CodabarLogicImpl logic;
         String expected;
-        
+
         logic = new CodabarLogicImpl(ChecksumMode.CP_AUTO);
         logic.generateBarcodeLogic(new MockClassicBarcodeLogicHandler(sb), "d123e");
         expected = "<BC>"
@@ -78,8 +78,43 @@ public class CodabarTest extends TestCase {
         sb.setLength(0);
         logic.generateBarcodeLogic(new MockClassicBarcodeLogicHandler(sb), "D123E");
         assertEquals(expected, sb.toString());
-        
+
         /**@todo Implement start/stop character checking */
+    }
+
+    public void testStartStopHandling() throws Exception {
+        StringBuffer sb = new StringBuffer();
+        CodabarLogicImpl logic;
+        String expected;
+
+        //With start/stop in the HR part
+        logic = new CodabarLogicImpl(ChecksumMode.CP_AUTO, true);
+        logic.generateBarcodeLogic(new MockClassicBarcodeLogicHandler(sb, true), "*12*");
+        expected = "<BC:*12*>"
+            + "<SBG:msg-char:*>B1W1B1W2B1W2B2</SBG>"
+            + "W1"
+            + "<SBG:msg-char:1>B1W1B1W1B2W2B1</SBG>"
+            + "W1"
+            + "<SBG:msg-char:2>B1W1B1W2B1W1B2</SBG>"
+            + "W1"
+            + "<SBG:msg-char:*>B1W1B1W2B1W2B2</SBG>"
+            + "</BC>";
+        assertEquals(expected, sb.toString());
+
+        //Without start/stop in the HR part
+        sb.setLength(0);
+        logic = new CodabarLogicImpl(ChecksumMode.CP_AUTO, false);
+        logic.generateBarcodeLogic(new MockClassicBarcodeLogicHandler(sb, true), "*12*");
+        expected = "<BC:12>"
+            + "<SBG:msg-char:*>B1W1B1W2B1W2B2</SBG>"
+            + "W1"
+            + "<SBG:msg-char:1>B1W1B1W1B2W2B1</SBG>"
+            + "W1"
+            + "<SBG:msg-char:2>B1W1B1W2B1W1B2</SBG>"
+            + "W1"
+            + "<SBG:msg-char:*>B1W1B1W2B1W2B2</SBG>"
+            + "</BC>";
+        assertEquals(expected, sb.toString());
     }
 
 }
