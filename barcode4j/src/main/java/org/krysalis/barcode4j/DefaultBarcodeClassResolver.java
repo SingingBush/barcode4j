0861b66560a74627e4a30979297f63e2dcb3e82b
singingbush <singingbush@hotmail.com>
2025-02-23 03:48:21 +0000

    update DefaultBarcodeClassResolver to register using Class extending ConfigurableBarcodeGenerator

--- barcode4j/src/main/java/org/krysalis/barcode4j/DefaultBarcodeClassResolver.java
+++ barcode4j/src/main/java/org/krysalis/barcode4j/DefaultBarcodeClassResolver.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.krysalis.barcode4j.impl.AbstractBarcodeBean;
+import org.krysalis.barcode4j.impl.ConfigurableBarcodeGenerator;
 import org.krysalis.barcode4j.tools.ZXingUtil;
 
 /**
@@ -39,41 +40,61 @@ public class DefaultBarcodeClassResolver implements BarcodeClassResolver {
      * Already registers a default set of implementations.
      */
     public DefaultBarcodeClassResolver() {
-        registerBarcodeClass("codabar", "org.krysalis.barcode4j.impl.codabar.Codabar", true);
-        registerBarcodeClass("code39", "org.krysalis.barcode4j.impl.code39.Code39", true);
-        registerBarcodeClass("code128", "org.krysalis.barcode4j.impl.code128.Code128", true);
-        registerBarcodeClass("ean-128", "org.krysalis.barcode4j.impl.code128.EAN128", true);
-        registerBarcodeClass("ean128", "org.krysalis.barcode4j.impl.code128.EAN128");
-        registerBarcodeClass("2of5", "org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5");
-        registerBarcodeClass("intl2of5", "org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5", true);
-        registerBarcodeClass("interleaved2of5", "org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5");
-        registerBarcodeClass("itf-14", "org.krysalis.barcode4j.impl.int2of5.ITF14", true);
-        registerBarcodeClass("itf14", "org.krysalis.barcode4j.impl.int2of5.ITF14");
-        registerBarcodeClass("ean-13", "org.krysalis.barcode4j.impl.upcean.EAN13", true);
-        registerBarcodeClass("ean13", "org.krysalis.barcode4j.impl.upcean.EAN13");
-        registerBarcodeClass("ean-8", "org.krysalis.barcode4j.impl.upcean.EAN8", true);
-        registerBarcodeClass("ean8", "org.krysalis.barcode4j.impl.upcean.EAN8");
-        registerBarcodeClass("upc-a", "org.krysalis.barcode4j.impl.upcean.UPCA", true);
-        registerBarcodeClass("upca", "org.krysalis.barcode4j.impl.upcean.UPCA");
-        registerBarcodeClass("upc-e", "org.krysalis.barcode4j.impl.upcean.UPCE", true);
-        registerBarcodeClass("upce", "org.krysalis.barcode4j.impl.upcean.UPCE");
-        registerBarcodeClass("postnet", "org.krysalis.barcode4j.impl.postnet.POSTNET", true);
-        registerBarcodeClass("royal-mail-cbc", "org.krysalis.barcode4j.impl.fourstate.RoyalMailCBC", true);
-        registerBarcodeClass("usps4cb", "org.krysalis.barcode4j.impl.fourstate.USPSIntelligentMail", true);
+        registerBarcodeClass("codabar", org.krysalis.barcode4j.impl.codabar.Codabar.class, true);
+        registerBarcodeClass("code39", org.krysalis.barcode4j.impl.code39.Code39.class, true);
+        registerBarcodeClass("code128", org.krysalis.barcode4j.impl.code128.Code128.class, true);
+        registerBarcodeClass("ean-128", org.krysalis.barcode4j.impl.code128.EAN128.class, true);
+        registerBarcodeClass("ean128", org.krysalis.barcode4j.impl.code128.EAN128.class);
+        registerBarcodeClass("2of5", org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5.class);
+        registerBarcodeClass("intl2of5", org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5.class, true);
+        registerBarcodeClass("interleaved2of5", org.krysalis.barcode4j.impl.int2of5.Interleaved2Of5.class);
+        registerBarcodeClass("itf-14", org.krysalis.barcode4j.impl.int2of5.ITF14.class, true);
+        registerBarcodeClass("itf14", org.krysalis.barcode4j.impl.int2of5.ITF14.class);
+        registerBarcodeClass("ean-13", org.krysalis.barcode4j.impl.upcean.EAN13.class, true);
+        registerBarcodeClass("ean13", org.krysalis.barcode4j.impl.upcean.EAN13.class);
+        registerBarcodeClass("ean-8", org.krysalis.barcode4j.impl.upcean.EAN8.class, true);
+        registerBarcodeClass("ean8", org.krysalis.barcode4j.impl.upcean.EAN8.class);
+        registerBarcodeClass("upc-a", org.krysalis.barcode4j.impl.upcean.UPCA.class, true);
+        registerBarcodeClass("upca", org.krysalis.barcode4j.impl.upcean.UPCA.class);
+        registerBarcodeClass("upc-e", org.krysalis.barcode4j.impl.upcean.UPCE.class, true);
+        registerBarcodeClass("upce", org.krysalis.barcode4j.impl.upcean.UPCE.class);
+        registerBarcodeClass("postnet", org.krysalis.barcode4j.impl.postnet.POSTNET.class, true);
+        registerBarcodeClass("royal-mail-cbc", org.krysalis.barcode4j.impl.fourstate.RoyalMailCBC.class, true);
+        registerBarcodeClass("usps4cb", org.krysalis.barcode4j.impl.fourstate.USPSIntelligentMail.class, true);
         /* registerBarcodeClass("austpost", "org.krysalis.barcode4j.impl.fourstate.AustPost", true); */
-        registerBarcodeClass("pdf417", "org.krysalis.barcode4j.impl.pdf417.PDF417", true);
-        registerBarcodeClass("datamatrix", "org.krysalis.barcode4j.impl.datamatrix.DataMatrix", true);
+        registerBarcodeClass("pdf417", org.krysalis.barcode4j.impl.pdf417.PDF417.class, true);
+        registerBarcodeClass("datamatrix", org.krysalis.barcode4j.impl.datamatrix.DataMatrix.class, true);
 
         if (ZXingUtil.isZxingAvailable()) {
             // QR Code currently uses ZXing for encoding
-            final String clazz = org.krysalis.barcode4j.impl.qr.QRCode.class.getCanonicalName();
-            registerBarcodeClass("qr", clazz, true);
-            registerBarcodeClass("qrcode", clazz);
-            registerBarcodeClass("qr-code", clazz);
+            registerBarcodeClass("qr", org.krysalis.barcode4j.impl.qr.QRCode.class, true);
+            registerBarcodeClass("qrcode", org.krysalis.barcode4j.impl.qr.QRCode.class);
+            registerBarcodeClass("qr-code", org.krysalis.barcode4j.impl.qr.QRCode.class);
 
             // Aztec also requires ZXing
-            registerBarcodeClass("aztec", org.krysalis.barcode4j.impl.aztec.Aztec.class.getCanonicalName(), true);
+            registerBarcodeClass("aztec", org.krysalis.barcode4j.impl.aztec.Aztec.class, true);
         }
+    }
+
+    /**
+     * Registers a barcode implementation.
+     * @param id short name to use as a key
+     * @param clazz the class to register
+     * @param mainID indicates whether the name is the main name for the barcode
+     * @since 2.3.2
+     */
+    public <T extends ConfigurableBarcodeGenerator> void registerBarcodeClass(String id, Class<T> clazz, boolean mainID) {
+        registerBarcodeClass(id, clazz.getCanonicalName(), mainID);
+    }
+
+    /**
+     * Registers a barcode implementation.
+     * @param id short name to use as a key
+     * @param clazz the class to register
+     * @since 2.3.2
+     */
+    public <T extends ConfigurableBarcodeGenerator> void registerBarcodeClass(String id, Class<T> clazz) {
+        registerBarcodeClass(id, clazz.getCanonicalName(), false);
     }
 
     /**
@@ -81,6 +102,7 @@ public class DefaultBarcodeClassResolver implements BarcodeClassResolver {
      * @param id short name to use as a key
      * @param classname fully qualified classname
      */
+    @Deprecated
     public void registerBarcodeClass(String id, String classname) {
         registerBarcodeClass(id, classname, false);
     }
