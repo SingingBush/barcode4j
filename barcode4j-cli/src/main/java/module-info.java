ee071b31fbaf105fdb4256d84f6687be619f3029
singingbush <singingbush@hotmail.com>
2025-11-09 18:50:49 +0000

    #189: add module-info files (xalan module is still automatic module)

---
+++ barcode4j-cli/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author Samael Bate (singingbush)
+ * created on 16/10/2025
+ */
+module barcode4j.cli {
+
+    requires java.desktop;
+    requires java.xml;
+
+    requires barcode4j;
+
+    requires org.apache.commons.cli;
+
+    exports org.krysalis.barcode4j.cli;
+}
